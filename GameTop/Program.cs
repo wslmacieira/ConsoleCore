1a2397df37a97ae338eecf6628385a4d791f44d7
wslmacieiradev <wslmacieira@outlook.com>
2021-01-08 23:35:36 -0300

    Iniciando o Jogo

---
+++ GameTop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GameTop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFoda("Ronaldo");
+            jogo.IniciarJogo();
+        }
+    }
+
+    class JogoFoda
+    {
+        private readonly string _NomeJogador;
+
+        public JogoFoda(string nome)
+        {
+            _NomeJogador = nome;
+        }
+        public void IniciarJogo()
+        {
+            Console.WriteLine($"{_NomeJogador} deu um passe");
+        }
+    }
+}
